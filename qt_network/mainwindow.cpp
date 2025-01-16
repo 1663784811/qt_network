b3f6c0f8910a9b25d78ea66d045153e074865438
why <1663784811@qq.com>
2025-01-16 21:32:21 +0800

    init

--- qt_network/mainwindow.cpp
+++ qt_network/mainwindow.cpp
@@ -4,6 +4,9 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QScrollArea>
+#include <QTableView>
+
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow{parent}
 {
@@ -16,7 +19,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow{parent}
 
 void MainWindow::initView()
 {
-    QWidget *window = new QWidget;
+    QWidget *window = new QWidget();
     setCentralWidget(window);
     resize(1920,1080);
     // 获取屏幕尺寸
@@ -26,23 +29,46 @@ void MainWindow::initView()
     setGeometry((screenGeometry.width() - width()) / 2, (screenGeometry.height() - height()) / 2, width(), height());
 
 
-    // 创建若干个子窗口对象
-    QPushButton *button1 = new QPushButton("One");
-    QPushButton *button2 = new QPushButton("Two");
-    QPushButton *button3 = new QPushButton("Three");
-    QPushButton *button4 = new QPushButton("Four");
-    QPushButton *button5 = new QPushButton("Five");
+    QScrollArea* scrollLeft = new QScrollArea();
+    scrollLeft->setWidgetResizable(true);
+    scrollLeft->setFixedWidth(240);
+    scrollLeft->setContentsMargins(0,0,0,0);
+
+    QWidget *scrollContent = new QWidget();
+    QVBoxLayout* menuLayout = new QVBoxLayout(scrollContent);
+
+    for (int i = 0; i < 80; ++i) {
+        QPushButton* btn =  new QPushButton("aaa"+ QString::number(i));
+        menuLayout->addWidget(btn);
+    }
+    scrollContent->setLayout(menuLayout);
+    scrollLeft->setWidget(scrollContent);
 
 
 
+
+
+
+
+    //==================================================================
+    QScrollArea *mainContainer = new QScrollArea();
+    mainContainer->setWidgetResizable(true);
+    mainContainer->setContentsMargins(0,0,0,0);
+    QWidget *mainContent = new QWidget();
+    mainContainer->setWidget(mainContent);
+    QTableView* aa = new QTableView();
+
+
+    //mainContent->setLayout();
+
+
+    //==================================================================
     // 创建水平布局对象
-    QHBoxLayout *layout = new QHBoxLayout;
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->setContentsMargins(0,0,0,0);
     // 将子窗口添加到布局中
-    layout->addWidget(button1);
-    layout->addWidget(button2);
-    layout->addWidget(button3);
-    layout->addWidget(button4);
-    layout->addWidget(button5);
+    layout->addWidget(scrollLeft);
+    layout->addWidget(mainContainer);
 
     // 将水平布局设置给父窗口对象
     window->setLayout(layout);
